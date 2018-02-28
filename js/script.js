36e8ae9b65ca0b4295e3022b17dba8fd5f1c5cb5
Tomasz Chowaniec <35344311+TC-Code@users.noreply.github.com>
2018-02-28 20:29:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,8 @@ function getTriangleArea(a, h){
 	if (a <= 0 || h <= 0)  {
 		alert('Invalid entry!');
 } 
-	else {
 		return (a * h/2);
-    }
 }
 var triangle1Area = getTriangleArea(0, 15);
 var triangle2Area = getTriangleArea(5, 15);
 var triangle3Area = getTriangleArea(6, 12);
-
